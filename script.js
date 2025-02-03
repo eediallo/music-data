ecb6eb5db15a023538837a2c8b93f5120199d570
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-03 10:25:28 +0000

    populate user selection dropdown with options from USERS array

--- script.js
+++ script.js
@@ -4,9 +4,21 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIDs } from "./data.js";
+import { getUserIDs, USERS } from "./data.js";
+const selectEl = document.querySelector("#user-list");
+
+function createOption(user) {
+  const option = document.createElement("option");
+  option.value = user.id;
+  option.textContent = user.name;
+  return option;
+}
+
+function createAndREnderOpotionList() {
+  const optionList = USERS.map(createOption);
+  selectEl.append(...optionList);
+}
 
 window.onload = function () {
-  const users = getUserIDs();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
+  createAndREnderOpotionList();
 };
